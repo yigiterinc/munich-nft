d505fd580a015e3b2327d4b153b881bc6f5cad58
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2021-10-06 00:47:49 +0300

    feat: fetch assets of given address, add axios

--- src/App.js
+++ src/App.js
@@ -1,71 +1,99 @@
-import { useState, useEffect } from 'react'
-import Web3 from 'web3'
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import axios from "axios";
 
-import Home from './views/Home'
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 
-let web3
+import Home from "./views/Home";
+
+let web3;
+let seaport;
 
 function App() {
-  const [account, setAccount] = useState(null) // Wallet Address
-  const [balance, setBalance] = useState(null) // In Ether
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [account, setAccount] = useState(null); // Wallet Address
+	const [balance, setBalance] = useState(null); // In Ether
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(async () => {
-    updateUserData()
-  }, [account, balance])
+	useEffect(async () => {
+		updateUserData();
+	}, [account, balance]);
 
-  useEffect(async () => {
-    if (!web3) loadWeb3()
+	useEffect(async () => {
+		if (!web3) await loadWeb3();
+		if (!seaport) await openSeaport();
 
-    if (account) {
-      const newBalance = await fetchBalance()
-      setBalance(web3.utils.fromWei(newBalance, 'ether'))
-    }
-  }, [account])
+		if (account) {
+			const newBalance = await fetchBalance();
+			setBalance(web3.utils.fromWei(newBalance, "ether"));
+			fetchNfts(); // TODO this call should be made from somewhere else
+		}
+	}, [account]);
 
-  const fetchBalance = async () => {
-    const balance = await web3.eth.getBalance(account)
+	const fetchBalance = async () => {
+		const balance = await web3.eth.getBalance(account);
 
-    return balance
-  }
+		return balance;
+	};
 
-  const updateUserData = async () => {
-    await loadWeb3()
-    await loadAccount()
+	const openSeaport = () => {
+		/* seaport = new OpenSeaPort(Web3.givenProvider, {
+				networkName: Network.Main,
+			}); */
+	};
 
-    if (account && balance) {
-      setIsLoggedIn(true)
-    }
-  }
+	const fetchNfts = async () => {
+		let assetsObjects;
+		try {
+			assetsObjects = await axios.get(
+				`https://rinkeby-api.opensea.io/api/v1/assets?owner=${account}&order_direction=desc&offset=0&limit=20`
+			);
+			assetsObjects = assetsObjects.data;
+		} catch (e) {
+			console.log("retrying", e);
+			setTimeout(() => {
+				this.refreshData();
+			}, 1500);
+			return;
+		}
+		console.log(assetsObjects);
+	};
 
-  const loadWeb3 = async () => {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    } else {
-      window.alert('Non-ethereum browser detected. Try using MetaMask!')
-    }
+	const updateUserData = async () => {
+		await loadWeb3();
+		await loadAccount();
 
-    web3 = window.web3
-  }
+		if (account && balance) {
+			setIsLoggedIn(true);
+		}
+	};
 
-  const loadAccount = async () => {
-    // Returns the list of accounts that metamask is aware of
-    const accounts = await web3.eth.getAccounts()
-    setAccount(accounts[0])
-  }
+	const loadWeb3 = async () => {
+		if (window.ethereum) {
+			window.web3 = new Web3(window.ethereum);
+			await window.ethereum.enable();
+		} else if (window.web3) {
+			window.web3 = new Web3(window.web3.currentProvider);
+		} else {
+			window.alert("Non-ethereum browser detected. Try using MetaMask!");
+		}
 
-  return (
-    <Router path="/">
-      <Switch>
-        <Home loginWithMetamask={updateUserData} isLoggedIn={isLoggedIn} />
-      </Switch>
-    </Router>
-  )
+		web3 = window.web3;
+	};
+
+	const loadAccount = async () => {
+		// Returns the list of accounts that metamask is aware of
+		const accounts = await web3.eth.getAccounts();
+		setAccount(accounts[0]);
+	};
+
+	return (
+		<Router path="/">
+			<Switch>
+				<Home loginWithMetamask={updateUserData} isLoggedIn={isLoggedIn} />
+			</Switch>
+		</Router>
+	);
 }
 
-export default App
+export default App;
