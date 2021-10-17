ac24fd7c373f7f0c4a9aa0090d77d3e49c07dd9c
cemal <barisconur@gmail.com>
2021-10-17 18:07:29 +0300

    refactor: convert styled components into material-ui default one

--- src/components/Home/nav/BrandLogo.jsx
+++ src/components/Home/nav/BrandLogo.jsx
@@ -1,13 +1,27 @@
 import React from "react";
-import { Logo, Title } from "./NavbarElements";
+import { makeStyles } from "@material-ui/core/styles";
+
 import logoImage from "../../../assets/images/dummy-logo.png";
 
+const useStyles = makeStyles((theme) => ({
+	logo: {
+		display: "flex",
+		alignItems: "center",
+		textDecoration: "none",
+		color: "#000",
+	},
+	title: {
+		marginLeft: "17.5px",
+	},
+}));
+
 const BrandLogo = () => {
+	const classes = useStyles();
 	return (
-		<Logo href="">
+		<a className={classes.logo} href="">
 			<img src={logoImage} alt="logo" />
-			<Title class="nav-title">Dummy</Title>
-		</Logo>
+			<h2 className={classes.title}>Dummy</h2>
+		</a>
 	);
 };
 
