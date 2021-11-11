bd2b143e2b8ef009409b677604c55967265eb878
Yiğit Kemal Erinç <yigit.erinc@ozu.edu.tr>
2021-11-11 14:31:54 +0100

    feat: implement how it works

--- src/components/home/HowItWorks.jsx
+++ src/components/home/HowItWorks.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import Instruction from "./Instruction";
+import { makeStyles } from "@material-ui/core/styles";
+import { IoMdWallet } from "react-icons/io";
+import { BsDownload, BsCoin } from "react-icons/bs";
+import { BiPencil } from "react-icons/bi";
+
+const iconSize = 50;
+
+const instructions = [
+	{
+		label: "Connect your wallet",
+		icon: <IoMdWallet size={iconSize} />,
+		description:
+			"Connect your MetaMask wallet by clicking \"Connect Wallet\" button on top right corner",
+	},
+	{
+		label: "Import/Create NFTs",
+		icon: <BsDownload size={iconSize} />,
+		description:
+			"Import your NFTs from multiple marketplaces or create new ones",
+	},
+	{
+		label: "Customize your gallery",
+		icon: <BiPencil size={iconSize} />,
+		description:
+			"Display NFTs from multiple blockchains and customize its looks to create a unique gallery",
+	},
+	{
+		label: "Trade your NFTs",
+		icon: <BsCoin size={iconSize} />,
+		description: "Send offers to buy and sell NFTs",
+	},
+];
+
+const useStyles = makeStyles((theme) => ({
+	title: {
+		textAlign: "center",
+		fontSize: "30px",
+		fontWeight: "lighter",
+		letterSpacing: "2px",
+		textTransform: "uppercase",
+		marginBottom: "5vh",
+	},
+	instructionsContainer: {
+		display: "flex",
+		flexDirection: "row",
+		padding: "2vh 2vw 2vh 2vw",
+	},
+}));
 
 function HowItWorks() {
-  return (
-    <div>
-      
-    </div>
-  )
+	const classes = useStyles();
+
+	return (
+		<div>
+			<h5 className={classes.title}>How it Works</h5>
+			<div className={classes.instructionsContainer}>
+				{instructions.map((instruction, i) => (
+					<Instruction key={i} data={instruction} />
+				))}
+			</div>
+		</div>
+	);
 }
 
-export default HowItWorks
+export default HowItWorks;
--- src/components/home/Instruction.jsx
+++ src/components/home/Instruction.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box, Typography } from "@material-ui/core";
 
-function Instruction() {
-  return (
-    <div>
-      
-    </div>
-  )
+const useStyles = makeStyles((theme) => ({
+	instructionContainer: {
+		height: "auto",
+		minHeight: "30vh",
+		width: "30vw",
+		boxShadow: "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 5px 5px",
+		borderRadius: "10px",
+		padding: "20px 10px",
+		marginRight: "2vw",
+	},
+	icon: {
+		textAlign: "center",
+		width: "100%",
+		marginBottom: "10px",
+		marginTop: "10px",
+	},
+	label: {
+		textAlign: "center",
+		marginBottom: "2vh",
+	},
+	descriptionContainer: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	description: {
+		width: "15vw",
+		fontSize: "17px",
+		textAlign: "center",
+	},
+}));
+
+function Instruction({ data }) {
+	const { icon, label, description } = data;
+	const classes = useStyles();
+
+	return (
+		<Box className={classes.instructionContainer}>
+			<div className={classes.icon}>{icon}</div>
+			<Typography variant="h5" className={classes.label}>
+				{label}
+			</Typography>
+			<div className={classes.descriptionContainer}>
+				<Typography variant="body1" className={classes.description}>
+					{description}
+				</Typography>
+			</div>
+		</Box>
+	);
 }
 
-export default Instruction
+export default Instruction;
