79a4e88e0db6b55ddc8d86487ed8ab5df2621807
cemal <barisconur@gmail.com>
2021-11-12 14:54:08 +0300

    feat: handle price field

--- src/components/nft-details/PriceField.jsx
+++ src/components/nft-details/PriceField.jsx
@@ -17,6 +17,13 @@ const useStyles = makeStyles({
 		justifyContent: "flex-end",
 		alignItems: "center",
 	},
+	makeOfferContainer: {
+		padding: "20px",
+		width: "50%",
+	},
+	makeOfferButton: {
+		width: "100%",
+	},
 	pricePanel: {
 		display: "flex",
 		marginRight: "auto",
@@ -38,40 +45,52 @@ const useStyles = makeStyles({
 	},
 });
 
-const PriceField = () => {
+const PriceField = (nftJson) => {
 	const classes = useStyles();
-	let dummyPrice = 1000;
-	let usdPrice = dummyPrice * 4800;
+	let price = null;
+	if (nftJson.price !== null) {
+		price = priceHelper(nftJson.price);
+	}
 	return (
-		<>
-			{dummyPrice ? (
-				<div className={classes.priceFieldBorder}>
-					<div className={classes.priceFieldContainer}>
-						<div className={classes.pricePanel}>
-							<div className={classes.priceBox}>
-								<Icon icon="mdi:ethereum" width="36" height="36" />
-								<Typography className={classes.price}>{dummyPrice}</Typography>
-							</div>
-							<Typography className={classes.usdPrice}>
-								{"($" + usdPrice + ")"}
-							</Typography>
+		<div className={classes.priceFieldBorder}>
+			{price !== null ? (
+				<div className={classes.priceFieldContainer}>
+					<div className={classes.pricePanel}>
+						<div className={classes.priceBox}>
+							<Icon icon="mdi:ethereum" width="36" height="36" />
+							<Typography className={classes.price}>{price}</Typography>
 						</div>
-						<div className={classes.buttonPanel}>
-							<Button
-								className={classes.buyButton}
-								variant="outlined"
-								color="primary"
-							>
-								buy now
-							</Button>
-						</div>
+						<Typography className={classes.usdPrice}>
+							{"($" + price * 4800 + ")"}
+						</Typography>
 					</div>
+					<Button
+						className={classes.buyButton}
+						variant="outlined"
+						color="primary"
+					>
+						buy now
+					</Button>
 				</div>
 			) : (
-				<div />
+				<div className={classes.makeOfferContainer}>
+					<>
+						<Button
+							className={classes.makeOfferButton}
+							variant="outlined"
+							color="primary"
+						>
+							make offer
+						</Button>
+					</>
+				</div>
 			)}
-		</>
+		</div>
 	);
+};
+
+export const priceHelper = (price) => {
+	return price.slice(0, price.lastIndexOf(".")) * 10 ** -18;
 };
 
 export default PriceField;
