7a770ab9be8c5ae3d18c47ee34292dcc404804fc
cemal <barisconur@gmail.com>
2021-12-04 21:14:32 +0300

    use the other common component

--- src/views/NftDetails.jsx
+++ src/views/NftDetails.jsx
@@ -20,6 +20,13 @@ const NftDetails = () => {
 	const classes = useStyles();
 	const [nftJson, setNftJson] = useState(null);
 	const { contractAddressId, tokenId } = useParams();
+	const [dataIsLoading, setDataIsLoading] = useState(true);
+
+	useEffect(() => {
+		if (nftJson) {
+			setDataIsLoading(false);
+		}
+	}, [nftJson]);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -51,9 +58,11 @@ const NftDetails = () => {
 
 	return (
 		<>
-			{
-				withSpinner(renderPage(classes, nftJson), nftJson)
-			}
+			{withSpinner(renderPage(classes, nftJson), dataIsLoading, {
+				position: "absolute",
+				left: "50%",
+				top: "50%",
+			})}
 		</>
 	);
 };
