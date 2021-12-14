add26793fea26e7c7645553c9e280a61b02750fb
cemal <barisconur@gmail.com>
2021-12-14 17:20:18 +0300

    refactor: remove unused like button functionality

--- src/components/nft-details/NftImage.jsx
+++ src/components/nft-details/NftImage.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardMedia from "@material-ui/core/CardMedia";
-import IconButton from "@material-ui/core/IconButton";
-
-import marketplaceLogo from "../../assets/images/openseaLogo.png";
-import { OPENSEA_BASE_URL } from "../../constants/openseaApiConstants";
 
 const useStyles = makeStyles({
 	nftImageContainer: {
@@ -30,34 +25,11 @@ const useStyles = makeStyles({
 
 const NftImage = (nftJson) => {
 	const classes = useStyles();
-	const [isLiked, setIsLiked] = useState(false);
-	const toggleLikeButton = () => setIsLiked(!isLiked);
 
 	return (
 		<div className={classes.nftImageContainer}>
-			{/* {renderHeader(classes, nftJson, isLiked, toggleLikeButton)} */}
 			{renderNftImage(classes, nftJson)}
 		</div>
-	);
-};
-
-const renderHeader = (classes, nftJson) => {
-	const contractAddressId = nftJson.contractAddressId;
-	const tokenId = nftJson.tokenId;
-	return (
-		<CardActions className={classes.header}>
-			<IconButton
-				href={OPENSEA_BASE_URL + "/" + contractAddressId + "/" + tokenId}
-				target="_blank"
-				rel="noopener noreferrer"
-				size="small"
-				disableRipple
-				disableFocusRipple
-				className={classes.marketButton}
-			>
-				<img src={marketplaceLogo} alt="market-logo" height={25} width={25} />
-			</IconButton>
-		</CardActions>
 	);
 };
 
