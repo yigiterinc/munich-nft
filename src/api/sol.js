21b05e1d31bfd5953dc1c31236a54801793ee0ab
Gülbike İmge Köksal <koksalg15@itu.edu.tr>
2022-01-27 20:06:03 +0100

    feat: fetch collectibles from sol wallet address

--- src/api/sol.js
+++ src/api/sol.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 import { SOL_NETWORK } from "../config/config";
-import { Connection, PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import {
+	getParsedNftAccountsByOwner,
+	createConnectionConfig,
+} from "@nfteyez/sol-rayz";
+
+const RPC_SERVER = {
+	devnet: "https://api.devnet.solana.com",
+};
 
 const connection = new Connection("https://api.devnet.solana.com");
 
@@ -15,8 +23,31 @@ export const fetchSolNftMetadata = async () => {
 	console.log(tokenmeta);
 };
 
-const RPC_SERVER = {
-	devnet: "http://api.devnet.solana.com",
+export const getAllNftDataByWalletAddress = async (solAddress) => {
+	const connect = createConnectionConfig(clusterApiUrl("devnet"));
+	const nfts = await getParsedNftAccountsByOwner({
+		publicAddress: solAddress,
+		connection: connect,
+		serialization: true,
+	});
+	return nfts;
+};
+
+export const getNftTokenData = async (solAddress) => {
+	try {
+		let nftData = await getAllNftDataByWalletAddress(solAddress);
+		var data = Object.keys(nftData).map((key) => nftData[key]);
+		let arr = [];
+		let n = data.length;
+		for (let i = 0; i < n; i++) {
+			console.log(data[i].data.uri);
+			let val = await axios.get(data[i].data.uri);
+			arr.push(val);
+		}
+		return arr;
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const fetchSolNftsByWalletAddress = async (solAddress) => {
