9cc590cf1e7984fa93888380b66a4b68c3a14d62
Yigit Kemal Erinc <erincyigit@gmail.com>
2022-01-27 20:48:17 +0100

    feat: fetch image url

--- src/api/sol.js
+++ src/api/sol.js
@@ -30,63 +30,12 @@ export const getAllNftDataByWalletAddress = async (solAddress) => {
 		connection: connect,
 		serialization: true,
 	});
+
+	const nftDetails = nfts.map((nft) => nft.data);
+	nftDetails.forEach(async (detail, i) => {
+		let metadata = await axios.get(detail.uri);
+		nfts[i].image = metadata.data.image;
+	});
+
 	return nfts;
-};
-
-export const getNftTokenData = async (solAddress) => {
-	try {
-		let nftData = await getAllNftDataByWalletAddress(solAddress);
-		var data = Object.keys(nftData).map((key) => nftData[key]);
-		let arr = [];
-		let n = data.length;
-		for (let i = 0; i < n; i++) {
-			console.log(data[i].data.uri);
-			let val = await axios.get(data[i].data.uri);
-			arr.push(val);
-		}
-		return arr;
-	} catch (error) {
-		console.log(error);
-	}
-};
-
-export const fetchSolNftsByWalletAddress = async (solAddress) => {
-	try {
-		const TOKEN_PROGRAM_ID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
-		const data = {
-			jsonrpc: "2.0",
-			id: 1,
-			method: "getTokenAccountsByOwner",
-			params: [
-				solAddress,
-				{
-					programId: TOKEN_PROGRAM_ID,
-				},
-				{
-					encoding: "jsonParsed",
-				},
-			],
-		};
-
-		const resp = await axios.post(RPC_SERVER[SOL_NETWORK], data);
-		const tokenAccountObjects = resp.data.result.value;
-		console.log(tokenAccountObjects);
-		const mints = tokenAccountObjects.map(
-			(tokenAcc) => tokenAcc.account.data.parsed.info.mint
-		);
-		const tokenListReq = await axios.get(
-			"https://cdn.jsdelivr.net/gh/solana-labs/token-list@main/src/tokens/solana.tokenlist.json"
-		);
-		const solanaTokensList = tokenListReq.data.tokens;
-
-		const metadataOfTokens = solanaTokensList.filter((tokenDescription) =>
-			mints.includes(tokenDescription.address)
-		);
-
-		console.log(metadataOfTokens);
-
-		return metadataOfTokens;
-	} catch (error) {
-		console.log(error);
-	}
 };
