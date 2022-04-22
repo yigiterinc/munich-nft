b567aceec12f2c2573d6509af270fabe69fdad51
Gülbike İmge Köksal <koksalg15@itu.edu.tr>
2022-04-23 01:08:21 +0300

    fix: get solana nft details

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const NETWORK_FOR_OPENSEA_API = "rinkeby"; // ! set to rinkeby or main, upper/lowercase matters
 export const ETH_NETWORK = "rinkeby"; // * network to be used while making calls to our contract
-export const SOL_NETWORK = "mainnet";
+export const SOL_NETWORK = "devnet"; // set to mainnet or devnet
 export const CONTRACT_ADDRESS_RINKEBY =
 	"0xE0622Ca84fe2aCD5a997579A1573c83F38a330C6"; // * address of deployed contract
 
